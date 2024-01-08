77fdda744ca8defb0361224759e3580b32e0d8e5
Ra <ranilsd86@gmail.com>
2024-01-08 18:10:07 -0500

    fixing errors for deployment

--- server/server.js
+++ server/server.js
@@ -35,15 +35,12 @@ app.use('/graphql', expressMiddleware(server, {
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-}
-
 
 db.once("open", () => {
   app.listen(PORT, () => {
@@ -51,6 +48,11 @@ db.once("open", () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
+
+}
+
+
+
 
 
 startApolloServer();
